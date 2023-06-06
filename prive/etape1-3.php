5a8b8ed8dc69a4066588647f738684bb91991510
Alex Goussian <alexgoussian@MacBook-Pro-de-Alex.local>
2023-06-06 14:23:00 +0200

    Déconnexion

--- prive/etape1-3.php
+++ prive/etape1-3.php
@@ -3,32 +3,33 @@ session_start(); // Démarrer la session
 ?>
 <!doctype html>
 <html lang="fr" dir="ltr" class="has-navbar-fixed-top">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <!-- obligatoire pour faire du responsive -->
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <script src="../javascript/menu.js"></script>
 
-        <title>Etape 1-3 - Simul'asso</title>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <!-- obligatoire pour faire du responsive -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <script src="../javascript/menu.js"></script>
 
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-        <link rel="manifest" href="/site.webmanifest">
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
-        <meta name="msapplication-TileColor" content="#da532c">
-        <meta name="theme-color" content="#ffffff">
+    <title>Etape 1-3 - Simul'asso</title>
 
-        <!-- framework css - Bulma -->
-        <link rel="stylesheet" href="../css/bulma.min.css?v=0.9.3">
-        <!-- media : screen signifie que cela s'applique à l'écran / print pour l'impression -->
-        <link rel="stylesheet" type="text/css" media="screen" href="../css/main.css?v=0.0.0">
-        <!-- on appelle d'abord la feuille de style bulma avant la notre car les lignes sont exécutées les unes
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+
+    <!-- framework css - Bulma -->
+    <link rel="stylesheet" href="../css/bulma.min.css?v=0.9.3">
+    <!-- media : screen signifie que cela s'applique à l'écran / print pour l'impression -->
+    <link rel="stylesheet" type="text/css" media="screen" href="../css/main.css?v=0.0.0">
+    <!-- on appelle d'abord la feuille de style bulma avant la notre car les lignes sont exécutées les unes
         après les autres et on veut que ce soit notre feuille qui prime -->
-    </head>
+</head>
 
-    <body>
+<body>
     <!-- HEADER -->
     <header>
         <?php
@@ -49,12 +50,11 @@ session_start(); // Démarrer la session
 
         <section class="section">
             <p class="petit">
-                L’activité indiquée servira de base à la rédaction de l’objet social. Vous pourrez le préciser dans un second temps 
+                L’activité indiquée servira de base à la rédaction de l’objet social. Vous pourrez le préciser dans un second temps
             </p>
 
             <form method="post" action="./../traitements/etape1/tt-etape1-3.php">
-                <input type="text" placeholder="Activité de l'association" class="input input-assoc"
-                       name="activite-association" required>
+                <input type="text" placeholder="Activité de l'association" class="input input-assoc" name="activite-association" required>
                 <br>
                 <br>
                 <button type="submit" class="bouton-bleu hover-blanc">Etape suivante</button>
@@ -93,5 +93,6 @@ session_start(); // Démarrer la session
     <aside class="is-fixed-bottom fond-bleu has-text-centered petit">
         © Pic'asso 2023 - Tous droits réservés
     </aside>
-    </body>
+</body>
+
 </html>
