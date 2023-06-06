3e45b3bfa4ae052ba65f053cbf6335e81ddac2cb
Alicia <vanhove.alicia1997@gmail.com>
2023-06-06 09:33:51 +0200

    connexion bdd avec pdo

--- configuration/base-de-donnees.php
+++ configuration/base-de-donnees.php
@@ -1,21 +1,21 @@
 <?php
 // Informations de connexion à la base de données
-$servername = "servd162127.srv.odns.fr"; // Nom du serveur MySQL
+$servername = "localhost"; // Nom du serveur MySQL
 $username = "simulasso_alicia"; // Nom d'utilisateur MySQL
 $password = "Ao-xY_chiW*M"; // Mot de passe MySQL
 $dbname = "simulasso_global"; // Nom de la base de données
 
-// Créer une connexion à la base de données
-$conn = new mysqli($servername, $username, $password, $dbname);
 
-// Vérifier la connexion
-if ($conn->connect_error) {
-    die("Erreur de connexion à la base de données : " . $conn->connect_error);
+try {
+    // Création d'une nouvelle instance PDO
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+    // Configuration des options de PDO
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo "Connexion à la base de données réussie!";
+} catch (PDOException $e) {
+    echo "Erreur de connexion à la base de données: " . $e->getMessage();
 }
 
-// Connexion réussie
-echo "Connexion à la base de données réussie";
-
-// Fermer la connexion
-$conn->close();
 
